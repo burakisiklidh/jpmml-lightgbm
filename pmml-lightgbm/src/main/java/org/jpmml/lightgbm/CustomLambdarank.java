4c9ad50cb8484df3544ce0a2e7547b14c74eec03
burakisiklidh <87756438+burakisiklidh@users.noreply.github.com>
2022-01-17 15:04:25 +0100

    Create CustomLambdarank.java

---
+++ pmml-lightgbm/src/main/java/org/jpmml/lightgbm/CustomLambdarank.java
@@ -0,0 +1,8 @@
+package org.jpmml.lightgbm;
+
+public class CustomLambdarank extends Regression {
+
+	public CustomLambdarank(String name, boolean average_output){
+		super(name, average_output);
+	}
+}
